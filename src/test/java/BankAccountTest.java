11357299022ccce6d0332724e74b418636157043
chandan.singh@scaler.com <chandan.singh@scaler.com>
2023-04-08 03:21:04 +0530

    updated test case

--- src/test/java/BankAccountTest.java
+++ src/test/java/BankAccountTest.java
@@ -36,106 +36,120 @@ void setUp() {
 
 }
     @Test
-    void accountNumberExists() {
+    void accountNumberExists() throws Exception {
         try {
             Field accountNumberField = BankAccount.class.getDeclaredField("accountNumber");
             assertEquals(accountNumberField.getType(), String.class);
-        } catch (NoSuchFieldException e) {
+        } catch (Exception e) {
             fail("accountNumber not found");
         }
     }
 
     @Test
-    void balanceExists() {
+    void balanceExists() throws Exception {
         try {
             Field balanceField = BankAccount.class.getDeclaredField("balance");
             assertEquals(balanceField.getType(), int.class);
-        } catch (NoSuchFieldException e) {
+        } catch (Exception e) {
             fail("balance not found");
         }
     }
 
     @Test
-    void roiExists() {
+    void roiExists() throws Exception {
         try {
             Field roiField = BankAccount.class.getDeclaredField("roi");
             assertEquals(roiField.getType(), double.class);
-        } catch (NoSuchFieldException e) {
+        } catch (Exception e) {
             fail("roi not found");
         }
     }
 
     @Test
-    void getSimpleInterestMethodExists(){
+    void getSimpleInterestMethodExists() throws Exception {
         try {
             Method getSimpleInterestMethod = bankAccount.getClass().getDeclaredMethod("getSimpleInterest", int.class);
-        } catch (NoSuchMethodException e) {
+        } catch (Exception e) {
             fail("get simple Interest method not found");
         }
     }
 
     @Test
-    void getSimpleInterestMethodSignatureCheck() throws NoSuchMethodException {
-        Method getSimpleInterestMethod = bankAccount.getClass().getDeclaredMethod("getSimpleInterest", int.class);
-        assertEquals(getSimpleInterestMethod.toString(), "double BankAccount.getSimpleInterest(int)");
+    void getSimpleInterestMethodSignatureCheck() throws Exception {
+        try {
+            Method getSimpleInterestMethod = bankAccount.getClass().getDeclaredMethod("getSimpleInterest", int.class);
+            assertEquals(getSimpleInterestMethod.toString(), "double BankAccount.getSimpleInterest(int)");
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
     @Test
-    void getBalanceWithInterestMethodExists(){
+    void getBalanceWithInterestMethodExists() throws Exception {
         try {
             Method getBalanceWithInterestMethod = bankAccount.getClass().getDeclaredMethod("getBalanceWithInterest", int.class);
-        } catch (NoSuchMethodException e) {
+        } catch (Exception e) {
             fail("get simple Interest method not found");
         }
     }
 
     @Test
-    void getBalanceWithInterestMethodSignatureCheck() throws NoSuchMethodException {
-        Method getBalanceWithInterestMethod = bankAccount.getClass().getDeclaredMethod("getBalanceWithInterest", int.class);
-        assertEquals(getBalanceWithInterestMethod.toString(), "double BankAccount.getBalanceWithInterest(int)");
-    }
-    @Test
-    void testGetSimpleInterest() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException, InvocationTargetException {
-        BankAccount account = new BankAccount();
+    void getBalanceWithInterestMethodSignatureCheck() throws Exception {
 
-        // Set balance using reflection
-        Field balanceField = BankAccount.class.getDeclaredField("balance");
-        balanceField.setAccessible(true);
-        balanceField.set(account, 1000);
-
-        // Set roi using reflection
-        Field roiField = BankAccount.class.getDeclaredField("roi");
-        roiField.setAccessible(true);
-        roiField.set(account, 5.0);
-
-        // Invoke getSimpleInterest method using reflection
-        Method getSimpleInterestMethod = BankAccount.class.getDeclaredMethod("getSimpleInterest", int.class);
-        getSimpleInterestMethod.setAccessible(true);
-        double interest1 = (double) getSimpleInterestMethod.invoke(account, 1);
-        double interest5 = (double) getSimpleInterestMethod.invoke(account, 5);
-        double interest0 = (double) getSimpleInterestMethod.invoke(account, 0);
-
-        // Assert the results
-        assertEquals(50.0, interest1, 0.0);
-        assertEquals(250.0, interest5, 0.0);
-        assertEquals(0.0, interest0, 0.0);
-    }
-
-    @Test
-    void testGetBalanceWithInterest() throws NoSuchFieldException, IllegalAccessException {
-        BankAccount account = new BankAccount();
-
-        // set the balance and roi fields using reflection
-        Field balanceField = account.getClass().getDeclaredField("balance");
-        balanceField.setAccessible(true);
-        balanceField.set(account, 1000);
-
-        Field roiField = account.getClass().getDeclaredField("roi");
-        roiField.setAccessible(true);
-        roiField.set(account, 5.0);
-
-        // call the getBalanceWithInterest() method using reflection
         try {
+            Method getBalanceWithInterestMethod = bankAccount.getClass().getDeclaredMethod("getBalanceWithInterest", int.class);
+            assertEquals(getBalanceWithInterestMethod.toString(), "double BankAccount.getBalanceWithInterest(int)");
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
+    }
+    @Test
+    void testGetSimpleInterest() throws Exception {
+        try {
+            BankAccount account = new BankAccount();
+
+            // Set balance using reflection
+            Field balanceField = BankAccount.class.getDeclaredField("balance");
+            balanceField.setAccessible(true);
+            balanceField.set(account, 1000);
+
+            // Set roi using reflection
+            Field roiField = BankAccount.class.getDeclaredField("roi");
+            roiField.setAccessible(true);
+            roiField.set(account, 5.0);
+
+            // Invoke getSimpleInterest method using reflection
+            Method getSimpleInterestMethod = BankAccount.class.getDeclaredMethod("getSimpleInterest", int.class);
+            getSimpleInterestMethod.setAccessible(true);
+            double interest1 = (double) getSimpleInterestMethod.invoke(account, 1);
+            double interest5 = (double) getSimpleInterestMethod.invoke(account, 5);
+            double interest0 = (double) getSimpleInterestMethod.invoke(account, 0);
+
+            // Assert the results
+            assertEquals(50.0, interest1, 0.0);
+            assertEquals(250.0, interest5, 0.0);
+            assertEquals(0.0, interest0, 0.0);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
+    }
+
+    @Test
+    void testGetBalanceWithInterest() throws Exception {
+        try {
+            BankAccount account = new BankAccount();
+
+            // set the balance and roi fields using reflection
+            Field balanceField = account.getClass().getDeclaredField("balance");
+            balanceField.setAccessible(true);
+            balanceField.set(account, 1000);
+
+            Field roiField = account.getClass().getDeclaredField("roi");
+            roiField.setAccessible(true);
+            roiField.set(account, 5.0);
+
+            // call the getBalanceWithInterest() method using reflection
+
             Method method = account.getClass().getDeclaredMethod("getBalanceWithInterest", int.class);
             method.setAccessible(true);
             double result1 = (double) method.invoke(account, 1);
@@ -147,66 +161,78 @@ void setUp() {
             assertEquals(1250.0, result2, 0.0);
             assertEquals(1000.0, result3, 0.0);
 
-        } catch (NoSuchMethodException | InvocationTargetException e) {
+        } catch (Exception e) {
             fail("getBalanceWithInterest method not found");
         }
     }
 
 
     @Test
-    public void testGetSimpleInterestWithZeroBalance() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        BankAccount account = new BankAccount();
+    public void testGetSimpleInterestWithZeroBalance() throws Exception {
+        try {
+            BankAccount account = new BankAccount();
 
-        Field balanceField = BankAccount.class.getDeclaredField("balance");
-        balanceField.setAccessible(true);
-        balanceField.set(account, 0);
+            Field balanceField = BankAccount.class.getDeclaredField("balance");
+            balanceField.setAccessible(true);
+            balanceField.set(account, 0);
 
-        Field roiField = BankAccount.class.getDeclaredField("roi");
-        roiField.setAccessible(true);
-        roiField.set(account, 5.0);
+            Field roiField = BankAccount.class.getDeclaredField("roi");
+            roiField.setAccessible(true);
+            roiField.set(account, 5.0);
 
-        Method getSimpleInterestMethod = BankAccount.class.getDeclaredMethod("getSimpleInterest", int.class);
-        getSimpleInterestMethod.setAccessible(true);
-        double interest = (double) getSimpleInterestMethod.invoke(account, 1);
+            Method getSimpleInterestMethod = BankAccount.class.getDeclaredMethod("getSimpleInterest", int.class);
+            getSimpleInterestMethod.setAccessible(true);
+            double interest = (double) getSimpleInterestMethod.invoke(account, 1);
 
-        assertEquals(0.0, interest, 0.0);
+            assertEquals(0.0, interest, 0.0);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
 
     @Test
-    public void testGetBalanceWithInterestWithZeroROI() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        BankAccount account = new BankAccount();
-        Field balanceField = BankAccount.class.getDeclaredField("balance");
-        balanceField.setAccessible(true);
-        balanceField.set(account, 1000);
+    public void testGetBalanceWithInterestWithZeroROI() throws Exception {
+        try {
+            BankAccount account = new BankAccount();
+            Field balanceField = BankAccount.class.getDeclaredField("balance");
+            balanceField.setAccessible(true);
+            balanceField.set(account, 1000);
 
-        Field roiField = BankAccount.class.getDeclaredField("roi");
-        roiField.setAccessible(true);
-        roiField.set(account, 0.0);
+            Field roiField = BankAccount.class.getDeclaredField("roi");
+            roiField.setAccessible(true);
+            roiField.set(account, 0.0);
 
-        Method getBalanceWithInterestMethod = BankAccount.class.getDeclaredMethod("getBalanceWithInterest", int.class);
-        getBalanceWithInterestMethod.setAccessible(true);
-        double balanceWithInterest = (double) getBalanceWithInterestMethod.invoke(account, 1);
+            Method getBalanceWithInterestMethod = BankAccount.class.getDeclaredMethod("getBalanceWithInterest", int.class);
+            getBalanceWithInterestMethod.setAccessible(true);
+            double balanceWithInterest = (double) getBalanceWithInterestMethod.invoke(account, 1);
 
-        assertEquals(1000.0, balanceWithInterest, 0.0);
+            assertEquals(1000.0, balanceWithInterest, 0.0);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
     @Test
-    public void testGetBalanceWithInterestWithNegativeTime() throws NoSuchFieldException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-        BankAccount account = new BankAccount();
-        Field balanceField = BankAccount.class.getDeclaredField("balance");
-        balanceField.setAccessible(true);
-        balanceField.set(account, 1000);
+    public void testGetBalanceWithInterestWithNegativeTime() throws Exception {
+        try {
+            BankAccount account = new BankAccount();
+            Field balanceField = BankAccount.class.getDeclaredField("balance");
+            balanceField.setAccessible(true);
+            balanceField.set(account, 1000);
 
-        Field roiField = BankAccount.class.getDeclaredField("roi");
-        roiField.setAccessible(true);
-        roiField.set(account, 5.0);
+            Field roiField = BankAccount.class.getDeclaredField("roi");
+            roiField.setAccessible(true);
+            roiField.set(account, 5.0);
 
-        Method getBalanceWithInterestMethod = BankAccount.class.getDeclaredMethod("getBalanceWithInterest", int.class);
-        getBalanceWithInterestMethod.setAccessible(true);
-        double balanceWithInterest = (double) getBalanceWithInterestMethod.invoke(account, -1);
+            Method getBalanceWithInterestMethod = BankAccount.class.getDeclaredMethod("getBalanceWithInterest", int.class);
+            getBalanceWithInterestMethod.setAccessible(true);
+            double balanceWithInterest = (double) getBalanceWithInterestMethod.invoke(account, -1);
 
-        assertEquals(950.0, balanceWithInterest, 0.0);
+            assertEquals(950.0, balanceWithInterest, 0.0);
+        } catch (Exception e) {
+            fail("Exception thrown: " + e);
+        }
     }
 
 }
